bf42f609172b40d9a88dab0570b6b8ab485ab447
nicokrym <nicokrym@gmail.com>
2025-03-17 12:14:15 -0300

    Variables Declaradas e Inicializadas

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    int cantidadAlumnos;
+    float largoMesaEnM;
+    string domicilioUsuario;
+    bool aceptaCondiciones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cantidadAlumnos = 28;
+        largoMesaEnM = 1.70f;
+        domicilioUsuario = "Libertador 5670";
+        aceptaCondiciones = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
